9947390cf2ec870fbb67fbec3ff2c891689d3dfb
Alexandre Cost <amgcosta95@gmail.com>
2024-10-08 10:40:41 +0100

    Exercicios sobre erros.

---
+++ 5. Errors/myerrors.py
@@ -0,0 +1,10 @@
+class RuntimeErrorWithCode(Exception):
+    """
+    Exception raised when a specific error code is needed.
+    """
+    def __init__(self, message, code):
+        super().__init__(f'Error code {code}: {message}')
+        self.code = code
+        
+        
+err = RuntimeErrorWithCode('An error happened.', 500)
